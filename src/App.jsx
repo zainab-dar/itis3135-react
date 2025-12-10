2fddf70e32d79c3c91f94d62a856cc854dc611e8
Zainab Dar <zainab.dar2007@gmail.com>
2025-12-09 23:48:43 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,6 @@ import Header from './components/Header';
 import Footer from './components/Footer';
 
 // Import Pages
-import Home from './pages/Home';
 import Introduction from './pages/Introduction';
 import Contract from './pages/Contract';
 import StudentList from './pages/StudentList';
