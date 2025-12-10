af00c3a3516c2f54df02388fa79e57d25363d961
Zainab Dar <zainab.dar2007@gmail.com>
2025-12-10 00:03:39 -0500

    commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+function Home() {
+    document.title = "Home - ITIS 3135";
+    return (
+        <main>
+            <h2>Home</h2>
+            <p>Welcome to my ITIS-3135 course site.</p>
+        </main>
+    );
+}
+
+export default Home;
