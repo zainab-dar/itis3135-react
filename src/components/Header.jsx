c2f4f9a77720332ef0101ec83251c381201ab70f
Zainab Dar <zainab.dar2007@gmail.com>
2025-12-10 00:18:18 -0500

    layout adjustments

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,9 +5,9 @@ function Header() {
         <header>
           <h1>Zainab Dar's Zen Dolphin | ITIS 3135</h1>
           <p className="tagline">Come along as I learn how to build my first webpage!</p>            <nav>
-                <Link to="/">Home</Link> |
-                <Link to="/introduction">Introduction</Link> |
-                <Link to="/contract">Contract</Link> |
+                <Link to="/">Home</Link> 
+                <Link to="/introduction">Introduction</Link> 
+                <Link to="/contract">Contract</Link> 
                 <Link to="/students">Student List</Link>
                 {/* The rest of the links are omitted as per assignment instructions */}
             </nav>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,8 +2,8 @@ function Home() {
     document.title = "Home - ITIS 3135";
     return (
         <main>
-            <h2>Home</h2>
-            <p>Welcome to my ITIS-3135 course site.</p>
+            <h2> ITIS 3135 Home </h2>  
+            <p> This is all the content for my ITIS 3135 class</p>
         </main>
     );
 }
